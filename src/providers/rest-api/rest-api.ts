1078a3ae49b4390cd29e7bc421ff03e3fa07728b
Henning Bertsch <henning.bertsch@q-perior.com>
2018-04-20 16:57:15 +0200

    showing json rest call result in view and started adding interfaces

--- src/providers/rest-api/rest-api.ts
+++ src/providers/rest-api/rest-api.ts
@@ -1,3 +1,4 @@
+import { Comment } from './../../interfaces/comment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
@@ -5,20 +6,25 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class RestApiProvider 
 {
-  testApi:string ="https://jsonplaceholder.typicode.com/posts";
+  testApi:string ="https://jsonplaceholder.typicode.com/comments?postId=1";
   restResult:Observable<any>;
+  resultComments:Comment[] = [];
 
   constructor(public client: HttpClient) 
   {    
-    
+
   }
 
   restCall()
   {
-    this.restResult = this.client.get(this.testApi);
-    this.restResult.subscribe(data => {
-      console.debug('Response data:',data);
-    })  
+    this.restResult = this.client.get<Comment>(this.testApi, {responseType:'json'});
+    this.restResult.subscribe(data => 
+      {
+        this.resultComments = data;
+        console.debug('Received comments: ', this.resultComments.length);
+        this.resultComments.forEach(element => {
+          console.debug('Email :',element.email);
+        });
+      })  
   }
-
 }
